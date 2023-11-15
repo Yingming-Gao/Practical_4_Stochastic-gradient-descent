a62b867a37585f60ca4f81d0a10cd6b1f01fa7aa
Yingming-Gao <145347326+Yingming-Gao@users.noreply.github.com>
2023-11-15 18:07:14 +0000

    Update train function

--- G26.r
+++ G26.r
@@ -24,11 +24,11 @@ netup <- function(d){
   
   ## Initialize empty lists for nodes values, weights and offset parameters.
   ## h is a list of nodes for each layer.
-  nn$h <- list()
+  nn$h <- vector("list", length = length(d))
   ## W is a list of weight matrices. 
-  nn$W <- list()
+  nn$W <- vector("list", length = length(d) - 1)
   ## b is a list of offset vectors.
-  nn$b <- list()
+  nn$b <- vector("list", length = length(d) - 1)
   
   ## Initialize node values of each layer with zero values.
   ## h[[l]] should be a vector contain the node values for layer l.
@@ -75,17 +75,42 @@ forward <- function(nn,inp){
 }
 
 
-# Define the function 'backward'.
+# Define the function 'backward' to compute the derivatives of loss.
 backward <- function(nn,k){
   ## nn is the undated network from 'forward'.
   ## k is the class.
-  ## This function computes the derivatives of the corresponding loss.
   
+  ## Get the number of layers, including input layer
+  L <- length(nn$h) 
   
+  nn$dW <- vector("list", length = L - 1)
+  nn$db <- vector("list", length = L - 1)
+  nn$dh <- vector("list", length = L)
   
-  return(list(dh=dh,dW=dW,db=db))
-  ## Return the list of derivatives w.r.t. the nodes, weights and offsets.
+  # Initialize derivatives of loss w.r.t the output layer
+  dh_l_1 <- exp(nn$h[[L]]) / sum(exp(nn$h[[L]]))
+  # The derivative w.r.t the node of class k is different
+  dh_l_1[k] <- dh_l_1[k] - 1
+
+  
+  # Back-propagation process
+  for (l in (L-1):1) {
+    # Due to ReLU transformation, node value could be 0
+    # The relevant derivatives should be set to 0
+    dh_l_1 <- dh_l_1 * (nn$h[[l+1]] > 0)
+    dh_l <- t(nn$W[[l]]) %*% dh_l_1
+    
+    nn$dh[[l]] <- dh_l
+    nn$dW[[l]] <- dh_l_1 %*% t(nn$h[[l]])
+    nn$db[[l]] <- dh_l_1
+    
+    dh_l_1 <- dh_l
+  }
+  
+  return(nn)
+  ## Update the list with derivatives w.r.t. the nodes, weights and offsets.
 }
+
 
 # Define the function 'train'.
 train <- function(nn,inp,k,eta=.01,mb=10,nstep=10000){
@@ -95,44 +120,9 @@ train <- function(nn,inp,k,eta=.01,mb=10,nstep=10000){
   ## mb is the number of data to randomly sample to compute the gradient. 
   ## nstep is the number of optimization steps to take.
   
+  ## Set a seed for random process.
+  set.seed(0)
   
-  
-  return(nn)
-  # Return the trained network.
-}
-
-# Finally, train a 4-8-7-3 network to classify irises to species 
-# based on the 4 characteristics given in the iris dataset in R. 
-
-
-# Classify the test data to species according to the class predicted.
-# Compute the misclassification rate.
-
-backward <- function(nn, k) {
-  L <- length(nn$W) + 1  # number of layers, including input
-  n <- length(nn$h[[L]]) # number of output nodes
-  
-  # Step 2: Initialize derivative of loss wrt output layer
-  dL_dh <- exp(nn$h[[L]]) / sum(exp(nn$h[[L]]))
-  dL_dh[k] <- dL_dh[k] - 1
-  
-  # Backpropagation
-  for (l in (L - 1):2) {
-    dh_next <- dL_dh
-    dh_current <- nn$W[[l - 1]] %*% dh_next
-    
-    relu_grad <- as.numeric(nn$h[[l]] > 0)
-    dL_dh <- relu_grad * dh_current
-    
-    nn$dh[[l - 1]] <- dL_dh
-    nn$dW[[l - 1]] <- dh_next %*% t(nn$h[[l - 1]])
-    nn$db[[l - 1]] <- dh_next
-  }
-  
-  return(nn)
-}
-
-train <- function(nn, inp, k, eta = 0.01, mb = 10, nstep = 10000) {
   n <- nrow(inp)
   
   for (step in 1:nstep) {
@@ -141,87 +131,69 @@ train <- function(nn, inp, k, eta = 0.01, mb = 10, nstep = 10000) {
     inp_mb <- inp[idx, ]
     k_mb <- k[idx]
     
-    # Initialize gradients
+    # Initialize a list to store batch gradients
+    batch <- list()
     for (l in 1:(length(nn$h) - 1)) {
-      nn$dh[[l]] <- matrix(0, nrow = ncol(nn$W[[l]]), ncol = mb)
-      nn$dW[[l]] <- matrix(0, nrow = nrow(nn$W[[l]]), ncol = ncol(nn$W[[l]]))
-      nn$db[[l]] <- rep(0, ncol(nn$W[[l]]))
+      batch$dW[[l]] <- matrix(0, nrow = nrow(nn$W[[l]]), ncol = ncol(nn$W[[l]]))
+      batch$db[[l]] <- matrix(0, nrow = nrow(nn$b[[l]]), ncol = ncol(nn$b[[l]]))
     }
     
-    # Compute gradients for each sample in minibatch
+    # Compute gradients for the entire minibatch
     for (i in 1:mb) {
-      nn <- forward(nn, inp_mb[i, ])
+      nn <- forward(nn, t(inp_mb[i, ]))
       nn <- backward(nn, k_mb[i])
+      
+      for (l in 1:(length(nn$h) - 1)) {
+        batch$dW[[l]] <- batch$dW[[l]] + nn$dW[[l]]
+        batch$db[[l]] <- batch$db[[l]] + nn$db[[l]]
+      }
     }
     
     # Update parameters
     for (l in 1:(length(nn$h) - 1)) {
-      nn$W[[l]] <- nn$W[[l]] - eta * nn$dW[[l]] / mb
-      nn$b[[l]] <- nn$b[[l]] - eta * nn$db[[l]] / mb
+      nn$W[[l]] <- nn$W[[l]] - eta * batch$dW[[l]] / mb
+      nn$b[[l]] <- nn$b[[l]] - eta * batch$db[[l]] / mb
     }
+    
   }
   
   return(nn)
+  # Return the trained network.
 }
 
 
+# Assume compute_loss function is defined to compute the loss of the model
+# based on the current parameters and the entire dataset.
 
+# Finally, train a 4-8-7-3 network to classify irises to species 
+# based on the 4 characteristics given in the iris dataset in R. 
+# import the iris data
+data ("iris")
 
-backward <- function(nn, k) {
-  # 初始化空列表以存储梯度
-  nn$dh <- vector("list", length(nn$h))
-  nn$dW <- vector("list", length(nn$W))
-  nn$db <- vector("list", length(nn$b))
-  d <- vector("list", length(nn$h))
-  # 计算损失函数对于 hL_j 的导数
-  q <- exp(nn$h[[length(nn$h)]])
-  nn$dh[[length(nn$dh)]] <- q / sum(q)
-  nn$dh[[length(nn$dh)]][k] <- nn$dh[[length(nn$dh)]][k] - 1
+# Divide the iris data into training data and test data
+test_input <- iris[seq(5, nrow(iris), by = 5), -ncol(iris)]
+training_input <- iris[-seq(5, nrow(iris), by = 5), -ncol(iris)]
+
+# Ensure test values are numeric
+test_out <- as.integer(iris[seq(5, nrow(iris), by = 5), ncol(iris)])
+training_out <- as.integer(iris[-seq(5, nrow(iris), by = 5), ncol(iris)])
   
-  # 对最后一层的导数进行ReLU判断
-  d[[length(nn$dh)]] <- pmax(0, nn$dh[[length(nn$dh)]]) 
+# Initialize the network
+d = matrix(c(4,8,7,3),ncol = 1)
+nn = netup(d)
+# Train the network
+nn = train(nn,training_input,training_out)
+
+# Classify the test data to species according to the class predicted.
+# Compute the misclassification rate.
+pre = matrix(0,nrow = 30, ncol = 1)
+for (i in 1:30){
+  a = t(as.matrix(test_input[i,]))
   
-  # 通过反向传播计算 Li 对于所有其他 hl_j 的导数
-  for (l in (length(nn$h)-1) :1) {
-    nn$dh[[l]] <-  t(nn$W[[l]]) %*% d[[l + 1]]
-    nn$dW[[l]] <- d[[l + 1]] %*% t(nn$h[[l]])
-    nn$db[[l]] <- d[[l + 1]]
-    d[[l]] <- pmax(0,t(nn$W[[l]]) %*% d[[l+1]] )
-    
-   
-  }
-  return(nn)
+  prediction = forward(nn,a)$h[[4]]
+  pre[i,1] = which.max(prediction)
 }
 
-
-
-train <- function(nn, inp, k, eta=.01, mb=10, nstep=10000) {
-  for (step in 1:nstep) {
-    # 随机选择 mb 个数据点
-    indices <- sample(1:nrow(inp), mb)
-    sampled_inp <- inp[indices,]
-    sampled_k <- k[indices]
-    
-    # 执行前向和后向传播
-    for (i in 1:mb) {
-      nn <- forward(nn, sampled_inp[i,])
-      nn <- backward(nn, sampled_k[i])
-      # 计算梯度的平均值并更新参数
-      for (l in 1:(length(nn$W))) {
-        nn$W[[l]] <- nn$W[[l]] - eta * nn$dW[[l]]
-        nn$b[[l]] <- nn$b[[l]] - eta * nn$db[[l]]
-      }
-    }
-  }
-    
-   
-  return(nn)
-  
-  
-  
-}
-
-
-
-
-
+# Print the misclassification rate
+misclassification_rate <- sum(pre != test_out) / length(test_out)
+cat("Misclassification Rate:", misclassification_rate, "\n")
